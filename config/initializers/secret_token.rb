48701301262857dc22537a833f3368a972a58904
Ubuntu <ubuntu@domU-12-31-39-12-39-BB.compute-1.internal>
2012-11-08 01:55:45 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '031eb511316c156ef78e8c11f5465d14e31444cf588c0f0ce121244d0f1839a77f9ee36655ceb813df1fc984bf7d27c3256127c88c357b0c6a71820aed752614'
